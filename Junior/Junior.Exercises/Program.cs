f3f0b3165856b937b17d4d2078b3fcbacc53aea4
hubsondev <hubson.dev@gmail.com>
2021-05-18 18:27:24 +0200

    Initial junior level structure

---
+++ Junior/Junior.Exercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Junior.Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
